5eada6eb0fe6224b0fadcc41aca9dd8d729e7420
bruno devesa <1130838@isep.ipp.pt>
2015-09-02 21:08:14 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name brunomeanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the brunomeanApp
+ */
+angular.module('brunomeanApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name brunomeanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the brunomeanApp
+ */
+angular.module('brunomeanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
